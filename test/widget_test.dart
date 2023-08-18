70456e5c9929478eb8ececfdd892f80d4e066701
Emrehan Dursun <emrehan.dursun@overtech.com.tr>
2023-08-18 08:45:14 +0300

    menu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:billapp/main.dart';
+import 'package:billapp/Main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
