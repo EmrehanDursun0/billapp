00e901757a6e8865f59c618d33ee4c91d7433f0d
Mehmet Zeynel Eroğlu <80847067+zeynel34@users.noreply.github.com>
2023-08-24 15:27:04 +0300

    Zeynel

---
+++ lib/MainFood/burgers_page.dart
@@ -0,0 +1,54 @@
+import 'package:billapp/MainFood/firebase/burgers_firebase.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BurgersPage extends StatefulWidget {
+  const BurgersPage({Key? key}) : super(key: key);
+
+  @override
+  State<BurgersPage> createState() => _BurgersPageState();
+}
+
+class _BurgersPageState extends State<BurgersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF260900),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        title: FittedBox(
+          child: Row(
+            children: [
+              Text(
+                'Ana Yemekler',
+                style: GoogleFonts.judson(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 100),
+              Text(
+                'Masa 1',
+                style: GoogleFonts.judson(
+                  fontSize: 26,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const BurgersFirebase(),
+    );
+  }
+}
---
+++ lib/MainFood/pizzas_page.dart
@@ -0,0 +1,54 @@
+import 'package:billapp/MainFood/firebase/pizzas_firebase.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PizzaPage extends StatefulWidget {
+  const PizzaPage({Key? key}) : super(key: key);
+
+  @override
+  State<PizzaPage> createState() => _PizzaPageState();
+}
+
+class _PizzaPageState extends State<PizzaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF260900),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        title: FittedBox(
+          child: Row(
+            children: [
+              Text(
+                'Pizzalar',
+                style: GoogleFonts.judson(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(width: 100),
+              Text(
+                'Masa 1',
+                style: GoogleFonts.judson(
+                  fontSize: 26,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: const PizzasFirebase(),
+    );
+  }
+}
