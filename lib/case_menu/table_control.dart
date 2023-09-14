8d1f538fb198cf91280a02b7a654b649ba37fd09
Mehmet Zeynel Eroğlu <80847067+zeynel34@users.noreply.github.com>
2023-09-14 14:42:18 +0300

    Zeynel
    TableControl Kısmı

--- lib/case_menu/table_control.dart
+++ lib/case_menu/table_control.dart
@@ -1,76 +1,138 @@
+import 'package:billapp/menu_upgrade/dynamic_menu_page.dart';
+import 'package:billapp/models/table.dart';
 import 'package:billapp/providers/table_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 Future<void> tablecontrol(BuildContext context, selectedtable) async {
+  final TableProvider tableProvider = context.read<TableProvider>();
+  final List<TableModel> allTables = tableProvider.allTables;
+
   await showDialog<void>(
     context: context,
     builder: (BuildContext context) {
-      final TableProvider tableProvider = context.watch<TableProvider>();
-      final allTables = tableProvider.allTables; // Tüm tabloları al
-
       return AlertDialog(
-        insetPadding: const EdgeInsets.all(20),
         backgroundColor: const Color(0xFFE0A66B),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
+          borderRadius: BorderRadius.circular(30),
         ),
-        content: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(5),
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF260900),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Center(
-                  child: Text(
-                    'Güncelleme Sayfası',
-                    style: GoogleFonts.judson(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(12),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: const Color(0xFF260900),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Center(
+                child: Text(
+                  "Masa Düzenle",
+                  style: GoogleFonts.judson(
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
-              ListView.builder(
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 12, bottom: 12),
+              height: 340,
+              width: MediaQuery.of(context).size.width,
+              child: ListView.builder(
                 shrinkWrap: true,
+                physics: const AlwaysScrollableScrollPhysics(),
                 itemCount: allTables.length,
                 itemBuilder: (BuildContext context, int index) {
-                  String tableName = allTables[index];
-                  return InkWell(
+                  final table = allTables[index];
+                  return ListTile(
                     onTap: () {
-                      selectedtable = tableName;
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      margin: const EdgeInsets.symmetric(vertical: 5),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF260900),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Text(
-                        tableName,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
+                      tableProvider.selectTable(table);
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const DynamicMenuPage(),
                         ),
+                      );
+                    },
+                    title: Text(
+                      table.name,
+                      style: GoogleFonts.judson(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   );
                 },
               ),
-              const SizedBox(height: 10),
-            ],
-          ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                addTable(context, allTables);
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                foregroundColor: Colors.black,
+                backgroundColor: const Color(0xFF260900),
+                fixedSize: const Size(140, 40),
+              ),
+              child: Text(
+                'Masa Ekle',
+                style: GoogleFonts.judson(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       );
     },
   );
+}
+
+Future<void> addTable(BuildContext context, List<TableModel> allTables) async {
+  int highestTableNumber = 0;
+  for (final table in allTables) {
+    final tableNumber =
+        int.tryParse(table.name.replaceAll(RegExp(r'[^0-9]'), ''));
+    if (tableNumber != null && tableNumber > highestTableNumber) {
+      highestTableNumber = tableNumber;
+    }
+  }
+
+  final newTableName = 'Masa ${highestTableNumber + 1}';
+
+  final newTable = TableModel(name: newTableName);
+  ddTable(newTable);
+
+  // Yeni masa seçme ve sayfaya yönlendirme
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => const DynamicMenuPage(),
+    ),
+  );
+}
+
+Future<void> ddTable(newTable) async {
+  final firestoreInstance = FirebaseFirestore.instance;
+  final mealRef = firestoreInstance.collection('tables');
+  final documentReference = mealRef.doc();
+  final id = documentReference.id;
+  final mealData = {
+    'name': newTable,
+    'id': id,
+  };
+  await documentReference.set(mealData);
 }
